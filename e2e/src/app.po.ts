7eb4c42fa5dcdb2cc15f707b392bc1861591053d
Rohan Thorat <rohanthorat@quickenloans.com>
2020-05-12 11:59:35 -0400

    initial work

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,22 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getTime(hour: number): string {
+    const time  = ((hour + 11) % 12 + 1);
+    let suffix: string;
+    // it is pm if hours from 12 onwards
+    (hour === 0) ? (suffix = 'am') : (suffix = (hour >= 12) ? 'pm' : 'am');
+
+    return time + suffix;
+  }
+
+  // getTime(hour: number): string {
+  //   const time  = ((hour + 11) % 12 + 1);
+  //   // let suffix: string;
+  //   // // it is pm if hours from 12 onwards
+  //   // (hour === 0) ? (suffix = 'am') : (suffix = (hour >= 12) ? 'pm' : 'am');
+
+  //   return time.toString();
+  // }
 }
